b9bc8fad1939950476efa93e384faf98d2493147
Ugur <ugurarslanpeace@gmail.com>
2023-05-05 08:09:47 +0300

    e.object added

--- script.js
+++ script.js
@@ -22,3 +22,7 @@ const closeModal = function () {
 // close model//
 btnClosedModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
+});
